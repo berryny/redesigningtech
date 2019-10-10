b8788ca36e5a179b5996844e0180ceea04670c08
J Desire <judi@redesigningtech.com>
2019-10-10 16:24:41 -0400

    Updates

---
+++ precache-manifest.b40fa48e0d15d6881732970d11fdd980.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc96dce4f00acc32c9b3bfe4a72de70e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed63fce9c4765726777e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "822cf33702922a84d617",
+    "url": "/static/css/main.57965c28.chunk.css"
+  },
+  {
+    "revision": "ed63fce9c4765726777e",
+    "url": "/static/js/2.c73263ab.chunk.js"
+  },
+  {
+    "revision": "822cf33702922a84d617",
+    "url": "/static/js/main.1b6c1220.chunk.js"
+  },
+  {
+    "revision": "b4d97e30dccc831dc66a",
+    "url": "/static/js/runtime-main.82902ad0.js"
+  },
+  {
+    "revision": "df59ea7886669eac2a9ea74354674cfb",
+    "url": "/static/media/img-Judi.df59ea78.jpg"
+  },
+  {
+    "revision": "cde71cde33247ecbc3614a474b83857d",
+    "url": "/static/media/jd-coder.cde71cde.jpg"
+  },
+  {
+    "revision": "2bd179dda1a55e2d793313e92bdbb808",
+    "url": "/static/media/jd-helpinghands-1.2bd179dd.jpg"
+  },
+  {
+    "revision": "982ec309dcb789d36b1a70da53c4a3fb",
+    "url": "/static/media/project-boogieblvd.982ec309.jpg"
+  },
+  {
+    "revision": "db85ec645f854e9c168bd75f1ad98852",
+    "url": "/static/media/project-stnicholashouses.db85ec64.jpg"
+  },
+  {
+    "revision": "c2c419186ada3af8921ee765f4ac4b00",
+    "url": "/static/media/project-ubbicycleadv.c2c41918.jpg"
+  },
+  {
+    "revision": "bbc3fd35504eb6eb8863f8a37402c5fb",
+    "url": "/static/media/project-vaccaroandwhite.bbc3fd35.jpg"
+  },
+  {
+    "revision": "ce4bba57de95ddfe5aa9dcc8c85654a4",
+    "url": "/static/media/project-williebgarden.ce4bba57.jpg"
+  }
+]);
