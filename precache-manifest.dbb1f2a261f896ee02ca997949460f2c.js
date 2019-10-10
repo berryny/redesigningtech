58f5ddcd64903de1b8deb04a7b0ebb34d8bb8629
J Desire <judi@redesigningtech.com>
2019-10-10 16:28:16 -0400

    Updates

---
+++ precache-manifest.dbb1f2a261f896ee02ca997949460f2c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9baec5f48cd3e7bcac504e5ede2d63",
+    "url": "/redesigningtech/index.html"
+  },
+  {
+    "revision": "ed63fce9c4765726777e",
+    "url": "/redesigningtech/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3a91625ba7d0c7f962cd",
+    "url": "/redesigningtech/static/css/main.59fb1a17.chunk.css"
+  },
+  {
+    "revision": "ed63fce9c4765726777e",
+    "url": "/redesigningtech/static/js/2.c73263ab.chunk.js"
+  },
+  {
+    "revision": "3a91625ba7d0c7f962cd",
+    "url": "/redesigningtech/static/js/main.1e0e2a16.chunk.js"
+  },
+  {
+    "revision": "975e70bbe22426368596",
+    "url": "/redesigningtech/static/js/runtime-main.5daf6a28.js"
+  },
+  {
+    "revision": "df59ea7886669eac2a9ea74354674cfb",
+    "url": "/redesigningtech/static/media/img-Judi.df59ea78.jpg"
+  },
+  {
+    "revision": "cde71cde33247ecbc3614a474b83857d",
+    "url": "/redesigningtech/static/media/jd-coder.cde71cde.jpg"
+  },
+  {
+    "revision": "2bd179dda1a55e2d793313e92bdbb808",
+    "url": "/redesigningtech/static/media/jd-helpinghands-1.2bd179dd.jpg"
+  },
+  {
+    "revision": "982ec309dcb789d36b1a70da53c4a3fb",
+    "url": "/redesigningtech/static/media/project-boogieblvd.982ec309.jpg"
+  },
+  {
+    "revision": "db85ec645f854e9c168bd75f1ad98852",
+    "url": "/redesigningtech/static/media/project-stnicholashouses.db85ec64.jpg"
+  },
+  {
+    "revision": "c2c419186ada3af8921ee765f4ac4b00",
+    "url": "/redesigningtech/static/media/project-ubbicycleadv.c2c41918.jpg"
+  },
+  {
+    "revision": "bbc3fd35504eb6eb8863f8a37402c5fb",
+    "url": "/redesigningtech/static/media/project-vaccaroandwhite.bbc3fd35.jpg"
+  },
+  {
+    "revision": "ce4bba57de95ddfe5aa9dcc8c85654a4",
+    "url": "/redesigningtech/static/media/project-williebgarden.ce4bba57.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b40fa48e0d15d6881732970d11fdd980.js"
+  "/redesigningtech/precache-manifest.dbb1f2a261f896ee02ca997949460f2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redesigningtech/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
