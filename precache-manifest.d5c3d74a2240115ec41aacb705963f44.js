0d57c1989e1eff83f417ad71f88e4f5ba51ab48d
J Desire <judi@redesigningtech.com>
2019-10-10 15:38:34 -0400

    Updates

---
+++ precache-manifest.d5c3d74a2240115ec41aacb705963f44.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe50ae4fb95ccaaa170de8c25a7c779",
+    "url": "/redesigningtech/index.html"
+  },
+  {
+    "revision": "ed63fce9c4765726777e",
+    "url": "/redesigningtech/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d694a28803d720c1dd11",
+    "url": "/redesigningtech/static/css/main.59fb1a17.chunk.css"
+  },
+  {
+    "revision": "ed63fce9c4765726777e",
+    "url": "/redesigningtech/static/js/2.c73263ab.chunk.js"
+  },
+  {
+    "revision": "d694a28803d720c1dd11",
+    "url": "/redesigningtech/static/js/main.e0e99406.chunk.js"
+  },
+  {
+    "revision": "975e70bbe22426368596",
+    "url": "/redesigningtech/static/js/runtime-main.5daf6a28.js"
+  },
+  {
+    "revision": "df59ea7886669eac2a9ea74354674cfb",
+    "url": "/redesigningtech/static/media/img-Judi.df59ea78.jpg"
+  },
+  {
+    "revision": "cde71cde33247ecbc3614a474b83857d",
+    "url": "/redesigningtech/static/media/jd-coder.cde71cde.jpg"
+  },
+  {
+    "revision": "2bd179dda1a55e2d793313e92bdbb808",
+    "url": "/redesigningtech/static/media/jd-helpinghands-1.2bd179dd.jpg"
+  },
+  {
+    "revision": "982ec309dcb789d36b1a70da53c4a3fb",
+    "url": "/redesigningtech/static/media/project-boogieblvd.982ec309.jpg"
+  },
+  {
+    "revision": "db85ec645f854e9c168bd75f1ad98852",
+    "url": "/redesigningtech/static/media/project-stnicholashouses.db85ec64.jpg"
+  },
+  {
+    "revision": "c2c419186ada3af8921ee765f4ac4b00",
+    "url": "/redesigningtech/static/media/project-ubbicycleadv.c2c41918.jpg"
+  },
+  {
+    "revision": "bbc3fd35504eb6eb8863f8a37402c5fb",
+    "url": "/redesigningtech/static/media/project-vaccaroandwhite.bbc3fd35.jpg"
+  },
+  {
+    "revision": "ce4bba57de95ddfe5aa9dcc8c85654a4",
+    "url": "/redesigningtech/static/media/project-williebgarden.ce4bba57.jpg"
+  }
+]);
